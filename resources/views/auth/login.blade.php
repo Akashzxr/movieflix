db27cd5ed5804ea817dbf539a36fade959f2a203
akashks <akashks8855@gmail.com>
2023-08-12 19:20:07 +0530

    redesigned login page into movieflix login template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,18 +1,18 @@
-<x-guest-layout>
-    <!-- Session Status -->
+ <?php /*<x-guest-layout>
+     //Session Status 
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
-        <!-- Email Address -->
+        //Email Address 
         <div>
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <!-- Password -->
+        // Password
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
@@ -24,7 +24,7 @@
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <!-- Remember Me -->
+        //Remember Me
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
@@ -44,4 +44,50 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> ?php> */ ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+<title>Netflix - Watch TV Shows Online, Watch Movies Online</title>
+@vite(['resources/css/login.css'])
+</head>
+<body>
+    <header class="showcase">
+        
+
+        <div class="logo">
+            <img src="images/logo.png">
+        </div>
+
+        <div class="showcase-content">
+            <div class="formm">
+                <form action="{{route('login')}}"  method="POST"  accept-charset="UTF-8">
+                    {{ csrf_field() }}
+                    <h1>Sign In</h1>
+                    <div class="info">
+                        <input class="email" type="email" placeholder="Email or phone number" name="email"> <br>
+                        <input class="email" type="password" placeholder="Password" name="password">
+                    </div>
+                    <div class="btn">
+                        <button class="btn-primary" type="submit">Sign In</button>
+                    </div>
+
+                </form>
+
+            </div>
+            
+            <div class="signup">
+                <p>New to Movieflix ?</p>
+                <a href="/signup">Sign up now</a>
+            </div>
+
+        </div>
+
+</header>
+</body>
+</html>
+
+
