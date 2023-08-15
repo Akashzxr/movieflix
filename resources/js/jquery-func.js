8c4be37d26249ecfdb1a5d3366c9c9640831a4cc
akashks <akashks8855@gmail.com>
2023-08-15 13:20:47 +0530

    changed user dashboard to the moviflix template and added logout icon

---
+++ resources/js/jquery-func.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	
+	$(".movie-image").hover(function(){
+		$(this).find(".play").css("display","flex");
+
+	},
+	function()
+	{
+		$(this).find(".play").hide();
+	});
+
+
+	$(".blink").focus(function() {
+            if(this.title==this.value) {
+                this.value = '';
+            }
+        })
+        .blur(function(){
+            if(this.value=='') {
+                this.value = this.title;                    
+			}
+		});
+});
---
+++ resources/js/script.js
@@ -0,0 +1,89 @@
+var swiper = new Swiper(".mc1", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".n1",
+      prevEl: ".p1",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
+
+  var swiper = new Swiper(".mc2", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".n2",
+      prevEl: ".p2",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
+
+  var swiper = new Swiper(".mc3", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".n3",
+      prevEl: ".p3",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
